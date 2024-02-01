c68143fc58d181bad4ee8f83e0c6f848d327f22a
Nadia <nadiapasha30@gmail.com>
2024-02-01 11:19:17 -0600

    model performance

--- serve.py
+++ serve.py
@@ -1,21 +1,40 @@
 import torch
 import torch.nn as nn
-import timm # pytorch library for image models
+#import timm # pytorch library for image models
 from transformers import AutoFeatureExtractor, ViTFeatureExtractor,ViTImageProcessor, ViTForImageClassification
 from torchvision import transforms
 from util import RockDataset
 from PIL import Image
 from torch.utils.data import DataLoader
+import os
+from sklearn.metrics import confusion_matrix
+import re
 
 
 
-processor = ViTImageProcessor.from_pretrained('/Users/nadiapasha/ML_prep/projects/RockClassification/rock_model')
-model = ViTForImageClassification.from_pretrained('/Users/nadiapasha/ML_prep/projects/RockClassification/rock_model')
+processor = ViTImageProcessor.from_pretrained('/Users/nadiapasha/ML_prep/projects/Rock/rock_model_small')
+model = ViTForImageClassification.from_pretrained('/Users/nadiapasha/ML_prep/projects/Rock/rock_model_small')
 
-image = Image.open('/Users/nadiapasha/ML_prep/projects/RockClassification/test/Amphibolite/Amphibolite4.png')
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-logits = outputs.logits
-# model predicts one of the 1000 ImageNet classes
-predicted_class_idx = logits.argmax(-1).item()
-print("Predicted class:", model.config.id2label[predicted_class_idx])
+y_true = []
+y_pred = []
+labels = []
+for class_ in os.listdir('/Users/nadiapasha/ML_prep/projects/Rock/test_data'):
+    if class_ != '.DS_Store':
+        labels.append(class_)
+        parent_dir = '/Users/nadiapasha/ML_prep/projects/Rock/test_data/'+class_
+        
+        for item in os.listdir(parent_dir): 
+            if item != '.DS_Store':
+
+                image = Image.open(parent_dir+'/'+item)
+                inputs = processor(images=image, return_tensors="pt")
+                outputs = model(**inputs)
+                logits = outputs.logits
+                # model predicts one of the classes
+                predicted_class_idx = logits.argmax(-1).item()
+                true = re.sub(r"\d+\.png", '', item)
+                y_true.append(true)
+                y_pred.append(model.config.id2label[predicted_class_idx])
+            #print("Predicted class:", model.config.id2label[predicted_class_idx])
+print('Labels: ',labels)
+print(confusion_matrix(y_true, y_pred, labels=labels))
